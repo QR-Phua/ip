5fa5d32c1e0b9114d9d36c370091fe8ee663b7a7
qing rong <76784887+QR-Phua@users.noreply.github.com>
2026-01-21 13:53:11 +0800

    Created Task Class
    Task class will handle all Task related events

---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String description;
+    private boolean done;
+
+    public Task(String description) {
+        this.description = description;
+        this.done = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public String getStatus() {
+        return (isDone() ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.done = true;
+    }
+
+    public void markNotDone() {
+        this.done = false;
+    }
+
+
+}
